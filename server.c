f9220e103d4034a9428f45e133ea10a2184d3e71
Joao Coutinho <j.afonso.coutinho@tecnico.ulisboa.pt>
2019-10-02 22:40:24 +0100

    Implements all UDP commands in server

--- server.c
+++ server.c
@@ -11,12 +11,17 @@
 #include <signal.h>
 #include <errno.h>
 #include <sys/wait.h>
+#include <dirent.h>
+#include <sys/stat.h>
 
 #define PORT "58044"
 #define BUFFER_SIZE 1024
+#define TOPIC_LEN 16
+#define QUESTION_LEN 19
 #define SMALL_BUFFER_SIZE 64
 
 int n, udp, tcp;
+int nTopics;
 fd_set rfds;
 socklen_t addrlen;
 struct addrinfo hints, *res;
@@ -151,8 +156,74 @@ int deleteNewLine(char *str)
     return c != NULL;
 }
 
+int countQuestions(char *dirName) {
+    DIR *dir;
+    struct dirent *entry;
+    int count = 0;
+
+    /*Opens directory*/
+    if (!(dir = opendir(dirName)))
+        error("Error opening directory");
+
+    /*Counts number questions in certain topic*/
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_REG && strchr(entry->d_name, '_') == NULL) {
+            count++;
+        }
+    }
+
+    closedir(dir);
+
+    return count;
+}
+
+int countAnswers(char *dirName, char *question) {
+    DIR *dir;
+    struct dirent *entry;
+    int count = 0;
+
+    /*Opens directory*/
+    if (!(dir = opendir(dirName)))
+        error("Error opening directory");
+
+    /*Coutns number of answers to a certain question*/
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_REG && strchr(entry->d_name, '_') != NULL &&
+                !strcmp(strtok(entry->d_name, "_"), strtok(question, "."))) {
+            count++;
+        }
+    }
+
+    closedir(dir);
+
+    return count;
+}
+
+int countTopics() {
+    DIR *dir;
+    struct dirent *entry;
+    int count = 0;
+
+    /*Opens topics directory*/
+    if (!(dir = opendir("topics")))
+        error("Error opening directory");
+
+    /*Cycles through all topics in directory*/
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_DIR) {
+            if (entry->d_name[0] == '.')
+                continue;
+            count++;
+        }
+    }
+
+    closedir(dir);
+
+    return count;
+}
+
 /*Performs register command and saves the reply in status*/
-void registerCommand(char *command, char *status)
+void registerCommand(char *status)
 {
     char *type = strtok(NULL, " ");
 
@@ -164,7 +235,156 @@ void registerCommand(char *command, char *status)
         strcpy(status, "RGR NOK\n");
 }
 
-void handleCommand(char *request, char *status)
+/*Lists available topics to send back to user*/
+void topicListCommand(char *response) {
+    DIR *dir;
+    struct dirent *entry;
+    char topics[99 * (TOPIC_LEN + 1)];
+
+    topics[0] = '\0';
+
+    /*Opens topics directory*/
+    if (!(dir = opendir("topics")))
+        error("Error opening directory");
+
+    /*Cycles through all topics in directory*/
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_DIR) {
+            if (entry->d_name[0] == '.')
+                continue;
+            strcat(topics, " ");
+            strcat(topics, entry->d_name);
+        }
+    }
+
+    closedir(dir);
+
+    if (nTopics == 0)
+        strcpy(response, "LTR 0\n");
+    else
+        sprintf(response, "LTR %d%s\n", nTopics, topics);
+}
+
+/*Returns the directory name of a certain topic*/
+void findTopic(char *topic, char *dirName) {
+    DIR *dir;
+    struct dirent *entry;
+    char *name, aux[TOPIC_LEN];
+
+
+    /*Opens topics directory*/
+    if (!(dir = opendir("topics")))
+        error("Error opening directory");
+
+    /*Cycles through all topics in directory*/
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_DIR) {
+            if (entry->d_name[0] == '.')
+                continue;
+
+            strcpy(aux, entry->d_name);
+            name = strtok(aux, ":");
+            if (!strcmp(topic, name)) {
+                strcpy(dirName, entry->d_name);
+                return;
+            }
+        }
+    }
+
+    closedir(dir);
+
+    dirName[0] = '\0';
+}
+
+/*Proposes topic to the forum*/
+void topicProposeCommand(char *response) {
+    char *userID = strtok(NULL, " ");
+    char *topic = strtok(NULL, " ");
+    char dirName[TOPIC_LEN + 8];
+    char testTopic[TOPIC_LEN];
+    int n;
+
+    deleteNewLine(topic);
+    findTopic(topic, testTopic);
+
+    /*Checks if id and topic are correct*/
+    if (strlen(userID) != 5 || strlen(topic) > 10) {
+        strcpy(response, "PTR NOK\n");
+        return;
+    } else if (nTopics == 99) {
+        strcpy(response, "PTR FUL\n");
+        return;
+    } else if (testTopic[0] != '\0') {
+        strcpy(response, "PTR DUP\n");
+        return;
+    }
+
+    sprintf(dirName, "topics/%s:%s", topic, userID);
+
+    /*Creates topic in directory*/
+    if ((n = mkdir(dirName, 0777)) == -1) {
+        error("Error creating dir");
+    } else {
+        strcpy(response, "PTR OK\n");
+        nTopics++;
+    }
+}
+
+/*Lists questions in a certain topic*/
+void questionListCommand(char *response) {
+    DIR *dir;
+    struct dirent *entry;
+    char *topic = strtok(NULL, " ");
+    char topicDir[TOPIC_LEN];
+    char dirName[TOPIC_LEN + 8];
+    char questions[99 * (QUESTION_LEN + 1)];
+    int nQuestions;
+
+    questions[0] = '\0';
+
+    deleteNewLine(topic);
+    findTopic(topic, topicDir);
+
+    if (topicDir[0] == '\0') {
+        strcpy(response, "LQR 0\n");
+        return;
+    }
+
+    sprintf(dirName, "topics/%s", topicDir);
+
+    /*Opens directory*/
+    if (!(dir = opendir(dirName)))
+        error("Error opening directory");
+
+    nQuestions = 0;
+
+    /*Cycles through all topics in directory*/
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_REG) {
+            if (strchr(entry->d_name, '_') == NULL) {
+                int nAnswers = countAnswers(dirName, entry->d_name);
+                printf("answers: %d\n", nAnswers);
+                char answerString[2];
+                sprintf(answerString, "%d", nAnswers);
+                strcat(questions, " ");
+                strcat(questions, strtok(entry->d_name, "."));
+                strcat(questions, ":");
+                strcat(questions, answerString);
+                nQuestions++;
+            }
+        }
+    }
+
+    closedir(dir);
+
+    if (nQuestions == 0) {
+        strcpy(response, "LQR 0\n");
+    } else {
+        sprintf(response, "LQR %d%s\n", nQuestions, questions);
+    }
+}
+
+void handleCommand(char *request, char *response)
 {
     char *type;
 
@@ -175,19 +395,19 @@ void handleCommand(char *request, char *status)
     // TODO: implement commented funtions
     if (!strcmp(type, "REG"))
     {
-        registerCommand(request, status);
+        registerCommand(response);
     }
-    else if (!strcmp(type, "LTP"))
+    else if (!strcmp(request, "LTP\n"))
     {
-        //topicListCommand();
+        topicListCommand(response);
     }
     else if (!strcmp(type, "PTP"))
     {
-        // topicProposeCommand(command);
+        topicProposeCommand(response);
     }
     else if (!strcmp(type, "LQU"))
     {
-        // questionListCommand();
+        questionListCommand(response);
     }
     else if (!strcmp(type, "GQU"))
     {
@@ -201,20 +421,22 @@ void handleCommand(char *request, char *status)
     }
     else
     {
-        strcpy(status, "ERR\n");
+        strcpy(response, "ERR\n");
     }
 }
 
 int main()
 {
     struct sigaction pipe, intr, child;
-    char status[BUFFER_SIZE];
+    char response[BUFFER_SIZE];
     char buffer[BUFFER_SIZE];
     int client;
     pid_t pid;
 
     udp = openUDP();
     tcp = openTCP();
+
+    nTopics = countTopics();
 
     //Protects against SIGPIPE
     memset(&pipe, 0, sizeof pipe);
@@ -248,6 +470,7 @@ int main()
         else if (FD_ISSET(udp, &rfds))
         {
             //Got message from udp server
+            memset(buffer, 0, sizeof buffer);
             addrlen = sizeof(addr);
             n = recvfrom(udp, buffer, 128, 0, (struct sockaddr *)&addr, &addrlen);
             if (n == -1)
@@ -259,8 +482,8 @@ int main()
             write(1, "udp received: ", 14);
             write(1, buffer, n);
 
-            handleCommand(buffer, status);
-            n = sendto(client, status, strlen(status), 0, (struct sockaddr *)&addr, addrlen);
+            handleCommand(buffer, response);
+            n = sendto(client, response, strlen(response), 0, (struct sockaddr *)&addr, addrlen);
             if (n == -1)
                 error("Error writing to udp socket");
         }
@@ -279,8 +502,8 @@ int main()
             else if (pid == 0) {
                 close(tcp);
                 readFromTCP(client, buffer);
-                handleCommand(buffer, status);
-                replyToTCP(status, client);
+                handleCommand(buffer, response);
+                replyToTCP(response, client);
                 close(client);
                 exit(EXIT_SUCCESS);
             }
